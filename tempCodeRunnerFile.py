6340a417fb4faa7e6cedf443bb5ab601a72cce3f
sergiuiacob1 <sergiu.iacob1@gmail.com>
2019-10-20 21:49:19 +0300

    few changes

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,4 @@
-import asyncio
-import cv2
-import time
-import json
-from pynput.mouse import Listener
-import logging
-from app import App
 
-
-def main():
-    app = App()
-    app.collectTrainingData()
+    def __init__(self, image, mousePosition):
+        self.image = image
+        self.mousePositi
--- webcam_capturer.py
+++ webcam_capturer.py
@@ -1,11 +1,16 @@
 import cv2
 import time
 import os
+import threading
+from multiprocessing import Process
 
 
 class WebcamCapturer:
-    def startCapturing(self):
+    def __init__(self):
         self.cam = cv2.VideoCapture(0)
+
+    def startCapturing(self):
+        ...
 
     def getWebcamImage(self):
         ret, frame = self.cam.read()
@@ -23,23 +28,24 @@ class WebcamCapturer:
         os.makedirs(path, exist_ok=True)
         secondsSinceEpoch = time.time()
         cv2.imwrite(f'{path}/{secondsSinceEpoch}.png', image)
-        # cv2.imwrite('ceva.png', image)
 
     def previewWebcam(self):
-        # TODO this should be async?
-        cv2.namedWindow('iClicker')
+        windowName = 'Webcam Preview'
+        fps = 60
+        cv2.namedWindow(windowName)
 
         while True:
             ret, frame = self.cam.read()
-            cv2.imshow("WHAT IS THIS", frame)
+            cv2.imshow(windowName, frame)
             if not ret:
                 break
-            k = cv2.waitKey(1)
+            k = cv2.waitKey(1000//fps)
 
             if k % 256 == 27:
                 # ESC pressed
                 print('Escape hit, closing...')
                 break
+        cv2.destroyWindow(windowName)
 
     def __del__(self):
         if hasattr(self, 'cam'):
@@ -49,5 +55,7 @@ class WebcamCapturer:
 
 if __name__ == '__main__':
     web = WebcamCapturer()
-    web.startCapturing()
-    web.saveCurrentWebcamImage('data')
+    web.previewWebcam()
+    input('Done...')
+    # web.startCapturing()
+    # web.saveCurrentWebcamImage('data')
