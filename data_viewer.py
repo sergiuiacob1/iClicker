ec3367c0641902d9931cda3874c3b1bf3380da7c
sergiuiacob1 <sergiu.iacob1@gmail.com>
2019-12-02 18:08:17 +0200

    paint circles for mouse position

--- data_viewer.py
+++ data_viewer.py
@@ -11,6 +11,21 @@ widget_positions = {
 }
 
 
+class CircleOverlay(QtWidgets.QWidget):
+    def __init__(self, center, parent):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.center = center
+        self.setFixedSize(parent.size())
+        # self.setAttribute(QtCore.Qt.WA_NoSystemBackground)
+        # self.setAttribute(QtCore.Qt.WA_TransparentForMouseEvents)
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.setPen(QtGui.QPen(QtCore.Qt.red, 20))
+        painter.drawEllipse(self.center[0], self.center[1], 20, 20)
+
+
 class DataViewer(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
@@ -42,12 +57,26 @@ class DataViewer(QtWidgets.QWidget):
         sample_image = self.build_sample_image(item.image)
         image_label.setPixmap(QtGui.QPixmap.fromImage(sample_image))
 
+        # Builds this widget as a child of image_label
         text_label = QtWidgets.QLabel(image_label)
-        text_label.setText(f'{str(item.mousePosition)}\nVertical: {item.vertical}\nHorizontal: {item.horizontal}')
+        text_label.setText(
+            f'{str(item.mousePosition)}\nVertical: {item.vertical}\nHorizontal: {item.horizontal}')
+
+        # Builds this widget as a child of image_label
+        center = self.get_scaled_mouse_position(item, (image_label.width(), image_label.height()))
+        _ = CircleOverlay(center=center, parent=image_label)
 
         self.layout().replaceWidget(
             self.layout().itemAt(widget_positions["image"]).widget(), image_label)
+
+        print(image_label.width())
         self.show()
+
+    def get_scaled_mouse_position(self, item, size):
+        width, height = size
+        scaleW = item.mousePosition[0] / item.screenSize[0]
+        scaleH = item.mousePosition[1] / item.screenSize[1]
+        return (width * scaleW, height * scaleH)
 
     def build_sample_image(self, cv2_image):
         """Receives an image that was captured using OpenCV.
