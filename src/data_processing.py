normalization bug fix

src/data_processing.py
@@ -152,22 +152,17 @@ def extract_faces(X):
     apply_function_per_thread(X, face_detector.extract_face)
     # TODO use the function above for the rest of the transformations. do before & after comparison and write comparison in report
     for i in range(0, len(X)):
-        # X[i] = face_detector.extract_face(X[i])
-        # resize this image
+    #     # X[i] = face_detector.extract_face(X[i])
+    #     # resize this image
         X[i] = resize_cv2_image(X[i], fixed_dim=(
             Config.FACE_WIDTH, Config.FACE_HEIGHT))
-        # also convert to grayscale, for now
+    #     # also convert to grayscale, for now
         X[i] = convert_to_gray_image(X[i])
     return X
 
 
 def normalize_data(data):
-    # TODO watch out, this uses fixed values
-    for i in range(0, len(data)):
-        data[i] = np.true_divide(data[i], 255)
-
-    # for i in range(0, len(data)):
-    #     data[i] = (np.array(data[i][0])/255, np.array(data[i][1])/255)
+    data = np.apply_along_axis(lambda x: np.true_divide(x, 255), 1, data)
 
 
 def save_processed_data(data, name='train_data.pkl'):

src/trainer.py
@@ -111,11 +111,12 @@ def train_model(train_parameters):
     #     "training_time": math.floor(end_time - start_time)
     # }
 
+
 def train_cnn_with_faces_keras():
     # Lazy import so the app starts faster
     from keras.models import Sequential
     from keras.layers import Dense, ReLU, Dropout, Conv2D, MaxPooling2D, Flatten
-    from keras.optimizers import RMSprop
+    from keras.optimizers import RMSprop, Adam
     from keras import backend as K
 
     print('Loading train data...')
@@ -132,18 +133,19 @@ def train_cnn_with_faces_keras():
                      input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(ReLU())
-    model.add(Conv2D(32, (3, 3)))
+    model.add(Conv2D(32, kernel_size=(3, 3)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(ReLU())
-    model.add(Conv2D(32, (3, 3)))
-    model.add(MaxPooling2D(pool_size=(5, 5)))
-    model.add(ReLU())
+    # model.add(Conv2D(64, kernel_size=(4, 4)))
+    # model.add(MaxPooling2D(pool_size=(3, 3)))
+    # model.add(ReLU())
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
     # model.add(Dropout(0.5))
     model.add(Dense(4, activation='softmax'))
 
-    model.compile(optimizer='adam',
+    opt = Adam(lr=0.015)
+    model.compile(optimizer=opt,
                   loss='categorical_crossentropy', metrics=['accuracy'])
     start_time = time.time()
     fit_history = model.fit(
@@ -155,6 +157,7 @@ def train_cnn_with_faces_keras():
         "trained_with": "keras",
         "score": fit_history.history['loss']
     }
+
 
 def train_cnn_with_faces():
     import torch.nn as nn
@@ -298,7 +301,7 @@ def get_best_trained_model():
 
 if __name__ == '__main__':
     train_parameters = {
-        "epochs": 500
+        "epochs": 250
     }
     res = train_model(train_parameters)
     # res["fit_history"].history['loss']